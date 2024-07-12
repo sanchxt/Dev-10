eec6bb070257d2aa940335f1ebe301d91b983299
sanchxt <tpbeast007@gmail.com>
2024-07-13 03:26:30 +0530

    add cookieParser to middleware stack

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
-import express from "express";
 import dotenv from "dotenv";
+import express from "express";
+import cookieParser from "cookie-parser";
 
 import connectDB from "./config/db.js";
 import userRoutes from "./routes/userRoutes.js";
@@ -14,9 +15,9 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.use("/api/users", userRoutes);
-
 app.get("/", (req, res) => res.send("server is ready"));
 
 app.use(notFound);
