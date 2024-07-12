7dc26ff1b8a8354b6ef6da47ad572671868125a6
sanchxt <tpbeast007@gmail.com>
2024-07-13 03:09:51 +0530

    implement login and logout functions

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -7,7 +7,20 @@ import generateToken from "../utils/generateToken.js";
 // route    POST /api/users/auth
 // access   PUBLIC
 const authUser = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "auth user" });
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+  if (user && (await user.matchPassword(password))) {
+    generateToken(res, user._id);
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(401);
+    throw new Error("Invalid email or password");
+  }
 });
 
 // desc     register new user
@@ -46,7 +59,12 @@ const registerUser = asyncHandler(async (req, res) => {
 // route    POST /api/users/logout
 // access   PUBLIC
 const logoutUser = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "logout user" });
+  res.cookie("jwt", "", {
+    httpOnly: true,
+    expires: new Date(0),
+  });
+
+  res.status(200).json({ message: "User logged out" });
 });
 
 // desc     get user profile
