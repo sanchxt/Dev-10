f53fdecfdb514ecca201aae851751c981cf34188
sanchxt <tpbeast007@gmail.com>
2024-07-12 19:03:26 +0530

    use error middleware

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import userRoutes from "./routes/userRoutes.js";
 
 dotenv.config();
@@ -12,5 +13,8 @@ const app = express();
 app.use("/api/users", userRoutes);
 
 app.get("/", (req, res) => res.send("server is ready"));
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`server started on port ${port}`));
