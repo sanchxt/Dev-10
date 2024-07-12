d1ba0d3b19bd845cdff847af1d3fe2cf742233d1
sanchxt <tpbeast007@gmail.com>
2024-07-13 00:59:27 +0530

    implement connection with mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
 
-import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+import connectDB from "./config/db.js";
 import userRoutes from "./routes/userRoutes.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
 dotenv.config();
 
 const port = process.env.PORT || 5000;
 
+connectDB();
 const app = express();
 
 app.use("/api/users", userRoutes);
