9f5a0e34d0ddece74b414ae2182cede61dacaade
sanchxt <tpbeast007@gmail.com>
2024-07-15 02:00:59 +0530

    implement sign up schema

--- frontend/src/utils/schema.ts
+++ frontend/src/utils/schema.ts
@@ -4,3 +4,19 @@ export const loginSchema = z.object({
   email: z.string().email(),
   password: z.string().min(8),
 });
+
+export const signupSchema = z
+  .object({
+    email: z.string().email(),
+    confirmEmail: z.string().email(),
+    password: z.string().min(8),
+    confirmPassword: z.string().min(8),
+  })
+  .refine((data) => data.email === data.confirmEmail, {
+    message: "E-mails don't match",
+    path: ["confirmEmail"],
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords don't match",
+    path: ["confirmPassword"],
+  });
