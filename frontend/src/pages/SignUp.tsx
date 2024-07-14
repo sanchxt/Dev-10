99292635016019e0a460bb735d57aa8c47f71c02
sanchxt <tpbeast007@gmail.com>
2024-07-15 02:00:51 +0530

    create signup container UI

---
+++ frontend/src/pages/SignUp.tsx
@@ -0,0 +1,25 @@
+import PublicNavbar from "../components/shared/PublicNavbar";
+import SignupForm from "../components/Sign-Up/SignupForm";
+
+const SignUp = () => {
+  return (
+    <section className="w-screen h-screen overflow-hidden flex flex-col">
+      <div className="h-fit shadow-xl">
+        <PublicNavbar />
+      </div>
+
+      <div
+        className="w-full flex-grow flex items-center"
+        style={{
+          background: "url('/test-bg.webp')",
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "cover",
+        }}
+      >
+        <SignupForm />
+      </div>
+    </section>
+  );
+};
+
+export default SignUp;
