ad2639e803d478167defb051565b6f6b67ae4eea
sanchxt <tpbeast007@gmail.com>
2024-07-14 17:55:56 +0530

    define breakpoints

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,6 +2,15 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    screens: {
+      xs: "360px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+      "3xl": "1920px",
+    },
     extend: {},
   },
   plugins: [],
