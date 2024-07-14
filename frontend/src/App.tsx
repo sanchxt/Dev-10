11a4e4e65a6e0baba7231efbbb9df3317a891018
sanchxt <tpbeast007@gmail.com>
2024-07-14 17:57:58 +0530

    implement useTheme hook

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,19 @@
+import { useEffect } from "react";
+
+import { useTheme } from "./hooks/useTheme";
+import Login from "./pages/Login";
+
 const App = () => {
-  return <div>App</div>;
+  const { theme } = useTheme();
+  useEffect(() => {
+    document.body.className = theme === "LIGHT" ? "light-theme" : "dark-theme";
+  }, [theme]);
+
+  return (
+    <div className="w-full">
+      <Login />
+    </div>
+  );
 };
 
 export default App;
