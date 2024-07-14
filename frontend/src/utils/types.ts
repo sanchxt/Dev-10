7c3bb28b1f68454ca288b4c086202058fde05b72
sanchxt <tpbeast007@gmail.com>
2024-07-15 02:01:08 +0530

    implement sign up form type

--- frontend/src/utils/types.ts
+++ frontend/src/utils/types.ts
@@ -1,6 +1,7 @@
 import { z } from "zod";
-import { loginSchema } from "./schema";
+import { loginSchema, signupSchema } from "./schema";
 
 export type ThemeType = "LIGHT" | "DARK";
 
 export type LoginFormFields = z.infer<typeof loginSchema>;
+export type SignupFormFields = z.infer<typeof signupSchema>;
