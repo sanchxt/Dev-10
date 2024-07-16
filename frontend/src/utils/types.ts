bc5cef5fbc491dea502d4e8c432fe7b87050a7b3
sanchxt <tpbeast007@gmail.com>
2024-07-16 22:19:24 +0530

    create sidebar icon type

--- frontend/src/utils/types.ts
+++ frontend/src/utils/types.ts
@@ -1,7 +1,15 @@
 import { z } from "zod";
 import { loginSchema, signupSchema } from "./schema";
+import { ComponentType } from "react";
 
 export type ThemeType = "LIGHT" | "DARK";
 
 export type LoginFormFields = z.infer<typeof loginSchema>;
 export type SignupFormFields = z.infer<typeof signupSchema>;
+
+// sidebar icons
+interface SidebarIconProps {
+  size: number;
+  className: string;
+}
+export type SidebarIconType = ComponentType<SidebarIconProps>;
