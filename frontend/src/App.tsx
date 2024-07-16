068d3aba5eb0a9100751cca736d45859eb07655d
sanchxt <tpbeast007@gmail.com>
2024-07-16 18:32:40 +0530

    implement toast container

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,18 @@
 import { useEffect } from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 import { useTheme } from "./hooks/useTheme";
 import NotFoundPage from "./pages/NotFoundPage";
 import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
+import Home from "./pages/Home";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Login />,
+    element: <Home />,
     errorElement: <NotFoundPage />,
   },
   {
@@ -28,7 +31,12 @@ const App = () => {
     document.body.className = theme === "LIGHT" ? "light-theme" : "dark-theme";
   }, [theme]);
 
-  return <RouterProvider router={router} />;
+  return (
+    <>
+      <RouterProvider router={router} />
+      <ToastContainer />
+    </>
+  );
 };
 
 export default App;
