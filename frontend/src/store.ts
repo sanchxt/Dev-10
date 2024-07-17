423c2c13d1e2c96f298a28e40bb757ded70d81c2
sanchxt <tpbeast007@gmail.com>
2024-07-17 17:15:00 +0530

    add theme in the store

--- frontend/src/store.ts
+++ frontend/src/store.ts
@@ -1,16 +1,20 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 import authReducer from "./slices/authSlice";
+import themeReducer from "./slices/themeSlice";
 import { apiSlice } from "./slices/apiSlice";
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
+    theme: themeReducer,
     [apiSlice.reducerPath]: apiSlice.reducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(apiSlice.middleware),
   devTools: true,
 });
+
+export type RootState = ReturnType<typeof store.getState>;
 
 export default store;
