11ca530f55ed57eb670f98aa81dbb3172b7379b3
sanchxt <tpbeast007@gmail.com>
2024-07-17 17:13:47 +0530

    add submenu component for sidebar

--- frontend/src/components/shared/SubMenu.tsx
+++ frontend/src/components/shared/SubMenu.tsx
@@ -1,9 +1,14 @@
 import { useState } from "react";
 import { motion } from "framer-motion";
 import { IoIosArrowDown } from "react-icons/io";
-import { NavLink, useLocation } from "react-router-dom";
+import { NavLink, useLocation, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
 
+import { logout } from "../../slices/authSlice";
+import { useLogoutMutation } from "../../slices/usersApiSlice";
 import { SidebarIconType } from "../../utils/types";
+import { loadTheme, setTheme } from "../../slices/themeSlice";
+import { RootState } from "../../store";
 
 interface SubMenuProps {
   data: {
@@ -16,6 +21,25 @@ interface SubMenuProps {
 const SubMenu = ({ data }: SubMenuProps) => {
   const { pathname } = useLocation();
   const [isSubMenuOpen, setIsSubMenuOpen] = useState<boolean>(false);
+
+  const dispatch = useDispatch();
+  const [logoutApiCall] = useLogoutMutation();
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    try {
+      await logoutApiCall().unwrap();
+      dispatch(logout());
+      navigate("/login");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const theme = useSelector((state: RootState) => state.theme.theme);
+  const handleToggleTheme = () => {
+    dispatch(setTheme(theme === "LIGHT" ? "DARK" : "LIGHT"));
+  };
 
   return (
     <>
@@ -48,9 +72,28 @@ const SubMenu = ({ data }: SubMenuProps) => {
       >
         {data.menu.map((subMenu) => (
           <li key={subMenu}>
-            <NavLink to="/" className="sidebar-link !bg-transparent capitalize">
-              {subMenu}
-            </NavLink>
+            {subMenu === "logout" ? (
+              <button
+                onClick={handleLogout}
+                className="sidebar-link !bg-transparent capitalize"
+              >
+                {subMenu}
+              </button>
+            ) : subMenu === "theme" ? (
+              <button
+                onClick={handleToggleTheme}
+                className="sidebar-link !bg-transparent capitalize"
+              >
+                {subMenu}
+              </button>
+            ) : (
+              <NavLink
+                to="/"
+                className="sidebar-link !bg-transparent capitalize"
+              >
+                {subMenu}
+              </NavLink>
+            )}
           </li>
         ))}
       </motion.ul>
