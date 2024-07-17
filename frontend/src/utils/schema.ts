4c7b5eccc402b366ca4f62ad1f53533b8c37fff0
sanchxt <tpbeast007@gmail.com>
2024-07-18 04:01:17 +0530

    add schema for update profile form

--- frontend/src/utils/schema.ts
+++ frontend/src/utils/schema.ts
@@ -18,3 +18,23 @@ export const signupSchema = z
     message: "Passwords don't match",
     path: ["confirmPassword"],
   });
+
+export const updateProfileSchema = z.object({
+  name: z
+    .string()
+    .min(3, { message: "Name must be at least 3 characters long" }),
+  email: z.string().email(),
+  oldPassword: z
+    .string()
+    .optional()
+    .refine((value) => !value || value.length >= 8, {
+      message: "Password is too short",
+    }),
+  newPassword: z
+    .string()
+    .optional()
+    .refine((value) => !value || value.length >= 8, {
+      message: "New password should be at least 8 characters long",
+    }),
+  description: z.string().optional(),
+});
