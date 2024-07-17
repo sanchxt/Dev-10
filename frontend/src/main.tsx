cb86c8af2570b958f5aad36a7c1d1396d5434926
sanchxt <tpbeast007@gmail.com>
2024-07-17 17:14:25 +0530

    remove theme context

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,14 +5,15 @@ import { Provider } from "react-redux";
 import store from "./store.ts";
 import App from "./App.tsx";
 import "./index.css";
-import { ThemeProvider } from "./context/ThemeContext.tsx";
+// import { ThemeProvider } from "./context/ThemeContext.tsx";
+import { loadTheme } from "./slices/themeSlice.ts";
+
+store.dispatch(loadTheme());
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <ThemeProvider>
-        <App />
-      </ThemeProvider>
+      <App />
     </Provider>
   </React.StrictMode>
 );
