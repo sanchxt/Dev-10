7e14bb5e4cd50352c17b4506c65333aae9fb122e
sanchxt <tpbeast007@gmail.com>
2024-07-17 17:15:38 +0530

    change signup schema to accept user name

--- frontend/src/utils/schema.ts
+++ frontend/src/utils/schema.ts
@@ -7,14 +7,12 @@ export const loginSchema = z.object({
 
 export const signupSchema = z
   .object({
+    name: z
+      .string()
+      .min(3, { message: "Name must be at least 3 characters long" }),
     email: z.string().email(),
-    confirmEmail: z.string().email(),
     password: z.string().min(8),
     confirmPassword: z.string().min(8),
-  })
-  .refine((data) => data.email === data.confirmEmail, {
-    message: "E-mails don't match",
-    path: ["confirmEmail"],
   })
   .refine((data) => data.password === data.confirmPassword, {
     message: "Passwords don't match",
