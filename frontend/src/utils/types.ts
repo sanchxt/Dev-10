0ecc1a8f14f32d60e396d4cde0365b9b5fe824b4
sanchxt <tpbeast007@gmail.com>
2024-07-18 04:01:27 +0530

    add type for user info stored in global state

--- frontend/src/utils/types.ts
+++ frontend/src/utils/types.ts
@@ -1,11 +1,12 @@
 import { z } from "zod";
-import { loginSchema, signupSchema } from "./schema";
+import { loginSchema, signupSchema, updateProfileSchema } from "./schema";
 import { ComponentType } from "react";
 
 export type ThemeType = "LIGHT" | "DARK";
 
 export type LoginFormFields = z.infer<typeof loginSchema>;
 export type SignupFormFields = z.infer<typeof signupSchema>;
+export type UpdateProfileFields = z.infer<typeof updateProfileSchema>;
 
 // sidebar icons
 interface SidebarIconProps {
@@ -13,6 +14,12 @@ interface SidebarIconProps {
   className: string;
 }
 export type SidebarIconType = ComponentType<SidebarIconProps>;
+
+export interface UserInfo {
+  _id: string;
+  name: string;
+  email: string;
+}
 
 export interface ApiError {
   data?: {
