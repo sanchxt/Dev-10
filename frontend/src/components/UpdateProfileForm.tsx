50c873ae14b745dbde3563833e9e07541fa48ff1
sanchxt <tpbeast007@gmail.com>
2024-07-19 03:45:54 +0530

    improve form UI, add theme toggle functionality

--- frontend/src/components/UpdateProfileForm.tsx
+++ frontend/src/components/UpdateProfileForm.tsx
@@ -1,20 +1,29 @@
-import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useDispatch, useSelector } from "react-redux";
 import { SubmitHandler, useForm } from "react-hook-form";
 
-import { updateProfileSchema } from "../utils/schema";
-import { ApiError, UpdateProfileFields, UserInfo } from "../utils/types";
-import { useUpdateUserMutation } from "../slices/usersApiSlice";
+import { setTheme } from "../slices/themeSlice";
 import { setCredentials } from "../slices/authSlice";
-import { toast } from "react-toastify";
+import { updateProfileSchema } from "../utils/schema";
+import {
+  ApiError,
+  ThemeType,
+  UpdateFieldConfig,
+  UpdateProfileFields,
+  UserInfo,
+} from "../utils/types";
+import { useUpdateUserMutation } from "../slices/usersApiSlice";
 
 const UpdateProfileForm = () => {
-  const navigate = useNavigate();
   const dispatch = useDispatch();
 
   const { userInfo } = useSelector(
     (state: { auth: { userInfo: UserInfo | null } }) => state.auth
+  );
+
+  const { theme } = useSelector(
+    (state: { theme: { theme: ThemeType } }) => state.theme
   );
 
   const [updateProfile, { isLoading }] = useUpdateUserMutation();
@@ -22,16 +31,12 @@ const UpdateProfileForm = () => {
   const {
     register,
     handleSubmit,
-    setError,
     formState: { errors, isSubmitting },
   } = useForm<UpdateProfileFields>({
     resolver: zodResolver(updateProfileSchema),
     defaultValues: {
       name: userInfo?.name || "",
       email: userInfo?.email || "",
-      oldPassword: "",
-      newPassword: "",
-      description: "",
     },
   });
 
@@ -53,6 +58,49 @@ const UpdateProfileForm = () => {
     }
   };
 
+  const updateProfileFields: UpdateFieldConfig[] = [
+    {
+      name: "name",
+      type: "text",
+      placeholder: "Your Name",
+      error: errors.name,
+      errorMessage: errors.name?.message,
+    },
+    {
+      name: "email",
+      type: "text",
+      placeholder: "E-mail",
+      error: errors.email,
+      errorMessage: errors.email?.message,
+    },
+    {
+      name: "oldPassword",
+      type: "text",
+      placeholder: "Old Password",
+      error: errors.oldPassword,
+      errorMessage: errors.oldPassword?.message,
+    },
+    {
+      name: "newPassword",
+      type: "text",
+      placeholder: "New Password",
+      error: errors.newPassword,
+      errorMessage: errors.newPassword?.message,
+    },
+    {
+      name: "description",
+      type: "textarea",
+      placeholder: "Description",
+      error: errors.description,
+      errorMessage: errors.description?.message,
+    },
+  ];
+
+  const handleToggleTheme = () => {
+    const newTheme = theme === "LIGHT" ? "DARK" : "LIGHT";
+    dispatch(setTheme(newTheme));
+  };
+
   return (
     <div className="w-full h-full flex flex-col text-black">
       <div className="text-center sm:py-1 px-1 xl:py-8 flex flex-col items-center gap-2 xl:gap-4">
@@ -66,54 +114,92 @@ const UpdateProfileForm = () => {
       </div>
 
       <div className="w-full flex-grow mt-2 flex justify-center md:items-center px-2 py-4">
-        <div className="w-full h-full md:w-2/3 md:h-3/4 bg-cyan-100/20 rounded-xl profile-inset-shadow">
-          <div className="w-full h-full bg-slate-400/80 rounded-tl-[5rem] rounded-xl profile-form-shadow text-gray-800">
-            <form onSubmit={handleSubmit(onSubmit)}>
-              <div>
-                <label htmlFor="name">Name</label>
-                <input {...register("name")} type="text" id="name" />
-                {errors.name && <p>{errors.name.message}</p>}
+        <div className="w-full h-full md:w-2/3 md:h-3/4 bg-purple-300/20 rounded-xl profile-inset-shadow">
+          <div className="w-full h-full bg-slate-400/80 rounded-tl-[5rem] rounded-xl profile-form-shadow text-gray-800 flex flex-col">
+            {/* theme toggle */}
+            <div className="p-2 flex justify-end">
+              <div className="inline-flex rounded-xl">
+                {" "}
+                {/* add relative here */}
+                {/* <span
+                  className={`absolute w-[30%] h-[2px] ${
+                    theme === "DARK"
+                      ? "bg-purple-400 left-[10%] top-[70%]"
+                      : "bg-purple-700 right-[10%] top-[70%]"
+                  } z-10`}
+                /> */}
+                <button
+                  onClick={handleToggleTheme}
+                  className={`w-1/2 transition-all duration-700 font-semibold ${
+                    theme === "DARK"
+                      ? "bg-gray-800 text-slate-100 underline decoration-purple-400 decoration-[1.5px] md:decoration-2 underline-offset-2 md:underline-offset-[3px]"
+                      : "bg-gray-500 text-gray-200 opacity-90 no-underline"
+                  } rounded-l-xl text-xs p-2 px-2.5 md:p-2.5 md:px-3 xl:px-4 xl:py-2.5`}
+                >
+                  Dark
+                </button>
+                <button
+                  onClick={handleToggleTheme}
+                  className={`w-1/2 transition-all duration-700 font-semibold ${
+                    theme === "LIGHT"
+                      ? "bg-slate-100 underline decoration-purple-700 decoration-[1.5px] md:decoration-2 underline-offset-2 md:underline-offset-[3px]"
+                      : "bg-gray-500 text-gray-200 opacity-90 no-underline"
+                  } rounded-r-xl text-xs p-2 px-2.5 md:p-2.5 md:px-3 xl:px-4 xl:py-2.5`}
+                >
+                  Light
+                </button>
               </div>
+            </div>
 
-              <div>
-                <label htmlFor="email">E-mail</label>
-                <input {...register("email")} type="text" id="email" />
-                {errors.email && <p>{errors.email.message}</p>}
+            <form
+              onSubmit={handleSubmit(onSubmit)}
+              className="pl-2 md:pl-4 flex-grow xl:grid xl:grid-cols-2"
+            >
+              {updateProfileFields.map((field) => (
+                <div
+                  className={`mx-2 md:mx-4 mt-4 ${
+                    field.name === "description" ? "xl:col-span-2" : ""
+                  }`}
+                  key={field.name}
+                >
+                  <div className="relative w-full group">
+                    {field.type === "textarea" ? (
+                      <textarea
+                        {...register(field.name)}
+                        id={field.name}
+                        placeholder={field.placeholder}
+                        className="w-full text-black text-sm bg-transparent p-2.5 border-0 border-b border-slate-800 hover:bg-purple-200/40 hover:rounded-t-xl focus:outline-none resize-none"
+                      />
+                    ) : (
+                      <input
+                        {...register(field.name)}
+                        type={field.type}
+                        id={field.name}
+                        placeholder={field.placeholder}
+                        className="w-full text-black text-sm bg-transparent p-2.5 border-0 border-b border-slate-800 hover:bg-purple-200/40 hover:rounded-t-xl focus:outline-none"
+                      />
+                    )}
+                    <span
+                      className={`absolute bg-blue-800 h-[2px] ${
+                        field.type === "textarea" ? "bottom-1" : "bottom-0"
+                      } right-0 transition-all duration-500 w-0 group-focus-within:w-full`}
+                    />
+                  </div>
+                  {field.error && (
+                    <p className="text-red-600 text-xs">{field.errorMessage}</p>
+                  )}
+                </div>
+              ))}
+
+              <div className="mx-2 md:mx-4 mt-4 text-center col-span-2">
+                <button
+                  type="submit"
+                  disabled={isSubmitting || isLoading}
+                  className="bg-gradient-to-r from-purple-300 via-purple-500 to-purple-300 text-white w-full p-2 rounded-xl text-xl"
+                >
+                  {isLoading ? "Loading..." : "Update Profile"}
+                </button>
               </div>
-
-              <div>
-                <label htmlFor="oldPassword">Old Password</label>
-                <input
-                  {...register("oldPassword")}
-                  type="text"
-                  id="oldPassword"
-                />
-                {errors.oldPassword && <p>{errors.oldPassword.message}</p>}
-              </div>
-
-              <div>
-                <label htmlFor="newPassword">New Password</label>
-                <input
-                  {...register("newPassword")}
-                  type="text"
-                  id="newPassword"
-                />
-                {errors.newPassword && <p>{errors.newPassword.message}</p>}
-              </div>
-
-              <div>
-                <label htmlFor="description">Description</label>
-                <input
-                  {...register("description")}
-                  type="text"
-                  id="description"
-                />
-                {errors.description && <p>{errors.description.message}</p>}
-              </div>
-
-              <button type="submit" disabled={isSubmitting || isLoading}>
-                {isLoading ? "Loading..." : "Update Profile"}
-              </button>
 
               {errors.root && <div>{errors.root.message}</div>}
             </form>
