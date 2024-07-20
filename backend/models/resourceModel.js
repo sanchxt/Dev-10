82d989d498537f6a785d0dc2a23e41511ec737b9
sanchxt <tpbeast007@gmail.com>
2024-07-21 02:42:59 +0530

    create schema for resource collection

---
+++ backend/models/resourceModel.js
@@ -0,0 +1,68 @@
+import mongoose from "mongoose";
+
+const resourceSchema = mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    tags: {
+      type: [String],
+      required: true,
+    },
+    essentials: {
+      type: [String],
+      required: true,
+    },
+    extras: {
+      type: [String],
+      required: true,
+    },
+    notes: {
+      type: String,
+      default: "",
+    },
+    ratings: [
+      {
+        user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+        rating: { type: Number, required: true },
+        comment: { type: String, required: true },
+      },
+    ],
+    averageRating: {
+      type: Number,
+      default: 0,
+    },
+    ratingBreakdown: {
+      type: Map,
+      of: Number,
+      default: {
+        1: 0,
+        2: 0,
+        3: 0,
+        4: 0,
+        5: 0,
+      },
+    },
+    favoritesCount: {
+      type: Number,
+      default: 0,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Resource = mongoose.model("Resource", resourceSchema);
+
+export default Resource;
