fa6fcff9d1060eb3480a7fa176756af7afa72353
sanchxt <tpbeast007@gmail.com>
2024-07-22 03:29:35 +0530

    create contribute resource page

---
+++ frontend/src/pages/ContributeResources.tsx
@@ -0,0 +1,15 @@
+import Sidebar from "../components/shared/Sidebar";
+import CreateResourceForm from "../components/contribute/CreateResourceForm";
+
+const ContributeResources = () => {
+  return (
+    <section className="flex flex-col md:flex-row h-screen">
+      <Sidebar />
+      <div className="flex-grow">
+        <CreateResourceForm />
+      </div>
+    </section>
+  );
+};
+
+export default ContributeResources;
