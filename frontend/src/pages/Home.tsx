e73a719f59bebaf30bf19a34e65f4b04cc2649ae
sanchxt <tpbeast007@gmail.com>
2024-07-25 00:41:40 +0530

    fix display issues

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -2,7 +2,7 @@ import Sidebar from "../components/shared/Sidebar";
 
 const Home = () => {
   return (
-    <section className="md:flex">
+    <section className="flex flex-col md:flex-row h-screen">
       <Sidebar />
       <div className="text-black bg-green-400 flex-grow">hi</div>
     </section>
