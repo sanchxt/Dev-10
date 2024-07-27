d57854f783c080107650813b0e9edb8c14991040
sanchxt <tpbeast007@gmail.com>
2024-07-27 16:43:02 +0530

    move no search result for resources component

--- frontend/src/components/resources/NoResourcesFound.tsx
+++ frontend/src/components/resources/NoResourcesFound.tsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 import { BiPlusCircle, BiSearch } from "react-icons/bi";
 
-import NoResults from "../assets/NoResults.gif";
+import NoResults from "../../assets/NoResults.gif";
 
 interface NoResourcesFoundProps {
   onExploreResources: () => void;
