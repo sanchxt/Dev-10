e6754cc250a2a1aefb81a187cd63878f55876ffa
sanchxt <tpbeast007@gmail.com>
2024-08-01 03:06:50 +0530

    display latest comments

--- frontend/src/components/resources/DetailedResource.tsx
+++ frontend/src/components/resources/DetailedResource.tsx
@@ -2,21 +2,37 @@ import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 
 import ReviewForm from "./ReviewForm";
+import { RootState } from "../../store";
 import DetailedHeader from "./DetailedHeader";
 import DetailedLinksAndNotes from "./DetailedLinksAndNotes";
-import { useGetResourceByIdQuery } from "../../slices/resourcesApiSlice";
-import { RootState } from "../../store";
+import {
+  useGetResourceByIdQuery,
+  useGetLatestCommentsQuery,
+} from "../../slices/resourcesApiSlice";
+import { LatestCommentsProps } from "../../utils/types";
 
 const DetailedResource = () => {
   const { id } = useParams<{ id: string }>();
   const { data, error, isLoading } = useGetResourceByIdQuery(id);
+  const {
+    data: comments,
+    error: commentsError,
+    isLoading: commentsLoading,
+  } = useGetLatestCommentsQuery(id);
+
   const currentUserId = useSelector(
     (state: RootState) => state?.auth?.userInfo?._id
   );
 
-  console.log(data?.user);
+  const filledComments = (comments || []).concat(
+    Array.from({ length: Math.max(0, 3 - (comments?.length || 0)) }).map(
+      () => ({
+        comment: "Encourage people to review this collection",
+        placeholder: true,
+      })
+    )
+  );
 
-  // author name, average rating, createdAt, description, essentials, extras, favoritesCount, isOfficial, notes, tags, title, updatedAt, user, _id
   return (
     <div className="text-black h-full">
       {isLoading ? (
@@ -47,21 +63,36 @@ const DetailedResource = () => {
 
               <div className="p-0.5 md:pt-1 xl:pt-2 px-1 md:px-2">
                 <div className="pt-1 md:pt-2 grid grid-cols-2 grid-rows-2 gap-1 gap-y-5">
-                  <div className="bg-gray-900 rounded-xl col-span-2 p-1.5 md:p-2 xl:p-2.5">
-                    <p className="text-[0.6rem] md:text-[0.68rem] xl:text-xs text-slate-200 font-medium overflow-hidden whitespace-nowrap text-ellipsis">
-                      Some random comment
-                    </p>
-                  </div>
-                  <div className="bg-gray-800 w-full rounded-xl p-1.5 md:p-2 xl:p-2.5">
-                    <p className="text-[0.6rem] md:text-[0.68rem] xl:text-xs text-gray-300 font-medium overflow-hidden whitespace-nowrap text-ellipsis">
-                      Some random comment 2
-                    </p>
-                  </div>
-                  <div className="bg-gray-800 w-full rounded-xl p-1.5 md:p-2 xl:p-2.5">
-                    <p className="text-[0.6rem] md:text-[0.68rem] xl:text-xs text-gray-300 font-medium overflow-hidden whitespace-nowrap text-ellipsis">
-                      Some random comment 3
-                    </p>
-                  </div>
+                  {commentsLoading ? (
+                    <div className="">Loading comments..</div>
+                  ) : commentsError ? (
+                    <div className="">
+                      Some error occured while trying to load the comments
+                    </div>
+                  ) : (
+                    filledComments.map(
+                      (comment: LatestCommentsProps, idx: number) => (
+                        <div
+                          key={idx}
+                          className={`${
+                            idx === 0
+                              ? "bg-gray-900 col-span-2"
+                              : "bg-gray-800 w-full"
+                          } rounded-xl p-1.5 md:p-2 xl:p-2.5`}
+                        >
+                          <p
+                            className={`text-[0.6rem] md:text-[0.68rem] xl:text-xs ${
+                              comment.placeholder
+                                ? "text-gray-300 italic"
+                                : "text-slate-200"
+                            } font-medium overflow-hidden whitespace-nowrap text-ellipsis`}
+                          >
+                            {comment.comment}
+                          </p>
+                        </div>
+                      )
+                    )
+                  )}
                 </div>
               </div>
             </div>
