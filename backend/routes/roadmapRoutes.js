9af893173a77d8a71057139212ab83fb0c8ccf4c
SeaSeekers <seaseekerssih@gmail.com>
2024-07-31 10:58:39 +0530

    roadmap schema updated

---
+++ backend/routes/roadmapRoutes.js
@@ -0,0 +1,24 @@
+import express from 'express';
+const router = express.Router();
+import {
+  // creatorName,
+  createRoadmap,
+  getAllRoadmaps,
+  getRoadmapById,
+  updateRoadmap,
+  deleteRoadmap,
+  rateRoadmap,
+} from '../controllers/roadmapController.js';
+import { protect } from '../middleware/authMiddleware.js';
+
+router.route('/').post(protect, createRoadmap).get(getAllRoadmaps);
+
+router
+  .route('/:id')
+  .get(getRoadmapById)
+  .put(protect, updateRoadmap)
+  .delete(protect, deleteRoadmap);
+
+router.route('/:id/rate').post(protect, rateRoadmap);
+
+export default router;
