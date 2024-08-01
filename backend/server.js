a5f8b3fe3611509483477ccf836c4cb90f0f2f00
sanchxt <tpbeast007@gmail.com>
2024-08-02 02:33:54 +0530

    add base path for resource reports

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import cookieParser from "cookie-parser";
 import connectDB from "./config/db.js";
 import userRoutes from "./routes/userRoutes.js";
 import resourceRoutes from "./routes/resourceRoutes.js";
+import resourceReportRoutes from "./routes/resourceReportRoutes.js";
 
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
@@ -22,6 +23,7 @@ app.use(cookieParser());
 
 app.use("/api/users", userRoutes);
 app.use("/api/resources", resourceRoutes);
+app.use("/api/resources/report", resourceReportRoutes);
 app.get("/", (req, res) => res.send("server is ready"));
 
 app.use(notFound);
