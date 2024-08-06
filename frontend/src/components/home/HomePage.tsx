a12f95dc475539642ad9a5e9d1ed22ce44224d04
sanchxt <tpbeast007@gmail.com>
2024-08-06 14:11:06 +0530

    create menu button for more notes

--- frontend/src/components/home/HomePage.tsx
+++ frontend/src/components/home/HomePage.tsx
@@ -6,6 +6,7 @@ import WelcomeBanner from "./WelcomeBanner";
 import { useState } from "react";
 import { useGetNotesQuery } from "../../slices/notesApiSlice";
 import DisplayNotes from "./DisplayNotes";
+import { RiMenu3Line } from "react-icons/ri";
 
 const HomePage = () => {
   const [isCreateNoteModalOpen, setIsCreateNoteModalOpen] =
@@ -26,14 +27,21 @@ const HomePage = () => {
         <div className="w-full grid md:grid-cols-2 xl:grid-cols-3">
           <div className="w-full xl:col-span-2 px-1 lg:pr-4 xl:pr-8">
             <h1 className="text-center py-1">Notes</h1>
-            <div className="w-full max-w-full bg-slate-100 rounded-lg grid grid-cols-2 lg:grid-cols-3 gap-1 p-1 lg:p-1.5">
+            <div className="w-full max-w-full bg-slate-100 rounded-lg p-1 lg:p-1.5">
               {isError ? (
                 <h1>Error fetching notes..</h1>
               ) : (
-                <DisplayNotes
-                  notes={data}
-                  handleCreateNote={handleCreateNote}
-                />
+                <div className="w-full max-w-full">
+                  <div className="flex justify-end p-2">
+                    <RiMenu3Line className="cursor-pointer" />
+                  </div>
+                  <div className="grid grid-cols-2 lg:grid-cols-3 gap-1">
+                    <DisplayNotes
+                      notes={data}
+                      handleCreateNote={handleCreateNote}
+                    />
+                  </div>
+                </div>
               )}
             </div>
           </div>
