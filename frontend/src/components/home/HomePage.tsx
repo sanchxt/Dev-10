182dc89587202a8282bf8b86f2e27eb3ef37ca9f
sanchxt <tpbeast007@gmail.com>
2024-08-06 13:38:52 +0530

    implement error handling

--- frontend/src/components/home/HomePage.tsx
+++ frontend/src/components/home/HomePage.tsx
@@ -4,6 +4,8 @@ import HomeHeader from "./HomeHeader";
 import AddNoteModal from "./AddNoteModal";
 import WelcomeBanner from "./WelcomeBanner";
 import { useState } from "react";
+import { useGetNotesQuery } from "../../slices/notesApiSlice";
+import DisplayNotes from "./DisplayNotes";
 
 const HomePage = () => {
   const [isCreateNoteModalOpen, setIsCreateNoteModalOpen] =
@@ -12,6 +14,8 @@ const HomePage = () => {
   const handleCreateNote = () => {
     setIsCreateNoteModalOpen(true);
   };
+
+  const { data, isError, isLoading, refetch } = useGetNotesQuery();
 
   return (
     <div className="px-1 h-full">
@@ -22,20 +26,15 @@ const HomePage = () => {
         <div className="w-full grid md:grid-cols-2 xl:grid-cols-3">
           <div className="w-full xl:col-span-2 px-1 lg:pr-4 xl:pr-8">
             <h1 className="text-center py-1">Notes</h1>
-            <div className="w-full max-w-full bg-slate-100 rounded-lg grid grid-cols-2 lg:grid-cols-3 gap-1 p-1">
-              <div className="bg-blue-200 h-32 lg:h-48 rounded-lg p-1">
-                <h2 className="text-[0.7rem] md:text-xs lg:text-sm">
-                  Sticky note title
-                </h2>
-              </div>
-              <div className="bg-blue-400 h-32 lg:h-48 rounded-lg">b</div>
-
-              <div
-                className="col-span-2 lg:col-span-1 bg-gray-400/80 h-32 lg:h-48 rounded-lg flex justify-center items-center cursor-pointer"
-                onClick={handleCreateNote}
-              >
-                <FaPlus className="text-gray-500 text-5xl bg-slate-300 border-black/30 border-2 rounded-full p-1" />
-              </div>
+            <div className="w-full max-w-full bg-slate-100 rounded-lg grid grid-cols-2 lg:grid-cols-3 gap-1 p-1 lg:p-1.5">
+              {isError ? (
+                <h1>Error fetching notes..</h1>
+              ) : (
+                <DisplayNotes
+                  notes={data}
+                  handleCreateNote={handleCreateNote}
+                />
+              )}
             </div>
           </div>
 
@@ -49,6 +48,7 @@ const HomePage = () => {
       <AddNoteModal
         isOpen={isCreateNoteModalOpen}
         onRequestClose={() => setIsCreateNoteModalOpen(false)}
+        requestRefetch={refetch}
       />
     </div>
   );
