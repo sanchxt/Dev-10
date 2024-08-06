139b4a35cf4b44fc6d9f5b6a4ac24286c76bc474
sanchxt <tpbeast007@gmail.com>
2024-08-07 03:14:33 +0530

    code improvements

--- frontend/src/components/home/HomeHeader.tsx
+++ frontend/src/components/home/HomeHeader.tsx
@@ -1,32 +1,25 @@
+import { memo } from "react";
 import { Link } from "react-router-dom";
+
+import { HomePageHeaderLinks } from "../../utils/constants";
 
 const HomeHeader = () => {
   return (
     <div className="flex justify-between items-center w-full">
       <div className="flex items-center md:gap-4 md:pl-8 lg:pl-12 xl:pl-16">
-        <div className="relative flex flex-col p-1 md:p-2">
-          <div className="relative w-fit flex flex-col group text-[0.7rem] md:text-base lg:text-lg xl:text-xl md:font-light tracking-wide lg:tracking-wider">
-            <Link
-              to="/resources"
-              className="text-center py-1 text-home-link-text theme-transition"
-            >
-              Explore Resources
-            </Link>
-            <span className="absolute bottom-0 w-0 group-hover:w-full transition-all duration-700 ease-in-out h-[0.1rem] md:h-0.5 bg-home-link-underline" />
+        {HomePageHeaderLinks.map((link) => (
+          <div className="relative flex flex-col p-1 md:p-2" key={link.id}>
+            <div className="relative w-fit flex flex-col group text-[0.7rem] md:text-base lg:text-lg xl:text-xl md:font-light tracking-wide lg:tracking-wider">
+              <Link
+                to={link.url}
+                className="text-center py-1 text-home-link-text theme-transition"
+              >
+                {link.text}
+              </Link>
+              <span className="absolute bottom-0 w-0 group-hover:w-full transition-all duration-700 ease-in-out h-[0.1rem] md:h-0.5 bg-home-link-underline" />
+            </div>
           </div>
-        </div>
-
-        <div className="relative flex flex-col p-1 md:p-2">
-          <div className="relative w-fit flex flex-col group text-[0.7rem] md:text-base lg:text-lg xl:text-xl md:font-light tracking-wide lg:tracking-wider">
-            <Link
-              to="/roadmaps"
-              className="text-center py-1 text-home-link-text theme-transition"
-            >
-              Explore Roadmaps
-            </Link>
-            <span className="absolute bottom-0 w-0 group-hover:w-full transition-all duration-700 ease-in-out h-[0.1rem] md:h-0.5 bg-home-link-underline" />
-          </div>
-        </div>
+        ))}
       </div>
 
       <img
@@ -37,4 +30,4 @@ const HomeHeader = () => {
   );
 };
 
-export default HomeHeader;
+export default memo(HomeHeader);
