8c126f3c70151a0c7fbc85197f7ed8b1dcf6d14c
sanchxt <tpbeast007@gmail.com>
2024-08-07 03:17:32 +0530

    create api slices to update and delete notes

--- frontend/src/slices/notesApiSlice.ts
+++ frontend/src/slices/notesApiSlice.ts
@@ -1,6 +1,6 @@
 import { apiSlice } from "./apiSlice";
 import { NOTES_URL } from "../utils/constants";
-import { CreateNoteProps } from "../utils/types";
+import { CreateNoteProps, UpdateNoteProps } from "../utils/types";
 
 export const notesApiSlice = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
@@ -16,8 +16,27 @@ export const notesApiSlice = apiSlice.injectEndpoints({
         url: `${NOTES_URL}`,
         method: "GET",
       }),
+      providesTags: ["Notes"],
+    }),
+    updateNote: builder.mutation<void, UpdateNoteProps>({
+      query: ({ id, color }) => ({
+        url: `${NOTES_URL}/${id}`,
+        method: "PUT",
+        body: { color },
+      }),
+    }),
+    deleteNote: builder.mutation<void, string>({
+      query: (id) => ({
+        url: `${NOTES_URL}/${id}`,
+        method: "DELETE",
+      }),
     }),
   }),
 });
 
-export const { useCreateNoteMutation, useGetNotesQuery } = notesApiSlice as any;
+export const {
+  useCreateNoteMutation,
+  useGetNotesQuery,
+  useUpdateNoteMutation,
+  useDeleteNoteMutation,
+} = notesApiSlice as any;
