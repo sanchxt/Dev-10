4450f2dd9d0cdde180b45915013354cf5d9d0dd4
SeaSeekers <116879428+vidushidubey0902@users.noreply.github.com>
2024-08-07 15:30:58 +0530

    theme

--- frontend/src/components/Sign-Up/SignupForm.tsx
+++ frontend/src/components/Sign-Up/SignupForm.tsx
@@ -53,7 +53,7 @@ const SignupForm = () => {
       errorMessage="E-mail has already been taken"
       welcomeText="Welcome!"
       additionalText="Welcome to the place made specifically for aspiring developers, like you, looking to take their skills to the next level."
-      sideImage="/login-gradient.webp"
+      sideImage="/card.jpg"
       bottomText="Already have an account? Login"
       redirectTo="/login"
       isLoading={isLoading}
