f8275969f27a8edc0148c2094d271a53a19a8dd3
Wriath18 <wriath18@gmail.com>
2024-08-07 18:13:02 +0530

    Chatbot made responsive

--- frontend/src/components/FloatingChatbotButton.tsx
+++ frontend/src/components/FloatingChatbotButton.tsx
@@ -1,22 +1,3 @@
-// import React from "react";
-// import "./FloatingChatbot.css"; // We'll create this CSS file next
-
-// const FloatingChatbot: React.FC = () => {
-//   return (
-//     <div className="floating-chatbot">
-//       <iframe
-//         src="https://dev10chatbot.streamlit.app/?embedded=true"
-//         title="Streamlit Chatbot"
-//         frameBorder="0"
-//         className="chatbot-iframe"
-//       ></iframe>
-//     </div>
-//   );
-// };
-
-// export default FloatingChatbot;
-
-// FloatingChatbotButton.tsx
 import React, { useState } from "react";
 import "./FloatingChatbotButton.css";
 
@@ -38,11 +19,10 @@ const FloatingChatbotButton: React.FC = () => {
         ></iframe>
       </div>
       <button className="chatbot-button" onClick={toggleChatbot}>
-        <img src="frontend\public\dev10-logo.webp" alt="Chatbot" className="chatbot-icon" />
+        <img src="/chatbot.png" alt="Chatbot" className="chatbot-icon" />
       </button>
     </>
   );
 };
 
 export default FloatingChatbotButton;
-
