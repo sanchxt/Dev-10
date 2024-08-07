98180e3634b2201329aa6db95d24e9bcbffe4da2
sanchxt <tpbeast007@gmail.com>
2024-08-07 14:26:12 +0530

    add bottom stats section

--- frontend/src/components/home/HomePage.tsx
+++ frontend/src/components/home/HomePage.tsx
@@ -3,12 +3,11 @@ import AddNoteModal from "./AddNoteModal";
 import WelcomeBanner from "./WelcomeBanner";
 import { useEffect, useState } from "react";
 import { useGetNotesQuery } from "../../slices/notesApiSlice";
-import DisplayNotes from "./DisplayNotes";
-import { RiMenu3Line } from "react-icons/ri";
 import ShowAllNotes from "./ShowAllNotes";
 import { useSelector } from "react-redux";
 import { RootState } from "../../store";
 import NotesSection from "./NotesSection";
+import { useTotalContributionsQuery } from "../../slices/usersApiSlice";
 
 const HomePage = () => {
   const [isCreateNoteModalOpen, setIsCreateNoteModalOpen] =
@@ -22,6 +21,13 @@ const HomePage = () => {
   const userInfo = useSelector((state: RootState) => state.auth.userInfo);
 
   const { data, isError, isLoading, refetch } = useGetNotesQuery();
+  const {
+    data: totalContributions,
+    isError: getContributionsError,
+    isLoading: getContributionsLoading,
+  } = useTotalContributionsQuery();
+
+  console.log(totalContributions);
 
   useEffect(() => {
     if (userInfo) {
@@ -30,11 +36,11 @@ const HomePage = () => {
   }, [userInfo]);
 
   return (
-    <div className="px-1 h-full">
+    <div className="px-1 h-full flex flex-col">
       <HomeHeader />
       <WelcomeBanner />
 
-      <div className="pt-4 w-full md:pl-2 lg:pl-4 xl:pl-6">
+      <div className="pt-4 w-full md:pl-2 lg:pl-4 xl:pl-6 pb-4">
         <div className="w-full grid md:grid-cols-2 xl:grid-cols-3">
           <div className="w-full xl:col-span-2 px-1 lg:pr-4 xl:pr-8">
             {/* <h1 className="text-center py-1">Notes</h1> */}
@@ -51,6 +57,38 @@ const HomePage = () => {
 
           <div className="w-full  bg-slate-300">
             <h1 className="text-center py-1">Game</h1>
+          </div>
+        </div>
+      </div>
+
+      <div className="md:px-2 lg:px-4 xl:px-6 pt-2 flex-grow h-fit grid gap-4 grid-cols-1 md:grid-cols-3">
+        <div className="flex items-end">
+          <div className="w-full h-full bg-gradient-to-br from-purple-300 to-purple-500 rounded-lg md:rounded-b-none"></div>
+        </div>
+        <div className="flex items-end">
+          <div className="w-full h-full md:h-[85%] bg-gradient-to-br from-purple-300 to-purple-500 rounded-lg md:rounded-b-none"></div>
+        </div>
+        <div className="flex items-end">
+          <div className="p-1 w-full h-full md:h-[70%] bg-gradient-to-br from-purple-300 to-purple-500 rounded-lg md:rounded-b-none flex justify-center items-center">
+            {getContributionsError ? (
+              <div>
+                <h3 className="italic font-light text-sm md:text-base xl:text-lg tracking-wider">
+                  Error fetching contributions..
+                </h3>
+                <h3 className="text-red-950 font-medium text-center">
+                  Try again later
+                </h3>
+              </div>
+            ) : getContributionsLoading ? (
+              <div className="italic tracking-widest animate-pulse text-base lg :text-lg">
+                <h3>Loading..</h3>
+              </div>
+            ) : (
+              <div>
+                <h1>Your Contributions</h1>
+                <h2 className="">{totalContributions?.totalContributions}</h2>
+              </div>
+            )}
           </div>
         </div>
       </div>
