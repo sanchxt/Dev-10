bf6303a04ebb82479fc37dab329a3e0b125f6950
sanchxt <tpbeast007@gmail.com>
2024-08-11 00:31:24 +0530

    add basic structure for landing page

--- frontend/src/pages/LandingPage.tsx
+++ frontend/src/pages/LandingPage.tsx
@@ -1,5 +1,17 @@
+import LandingHeader from "../components/landing-page/LandingHeader";
+
 const LandingPage = () => {
-  return <div>LandingPage</div>;
+  return (
+    <>
+      <LandingHeader />
+      <main>
+        <div>Hero Component</div>
+        <div>USPs</div>
+        <div>3 col layout</div>
+        <div>Carousel with posters</div>
+      </main>
+    </>
+  );
 };
 
 export default LandingPage;
