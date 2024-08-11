a3853a25192bf00051b9072765ef5db4db60ffe4
sanchxt <tpbeast007@gmail.com>
2024-08-11 23:25:59 +0530

    create paragraph component for landing page

---
+++ frontend/src/components/landing-page/LandingParagraph.tsx
@@ -0,0 +1,78 @@
+import { useRef } from "react";
+import { useScroll, motion, useTransform } from "framer-motion";
+
+import Container from "./Container";
+import { LandingWordsProps } from "../../utils/types";
+
+const LandingParagraph = ({ text }: { text: string }) => {
+  const element = useRef<HTMLParagraphElement | null>(null);
+  const { scrollYProgress } = useScroll({
+    target: element,
+    offset: ["start 0.9", "start 0.25"],
+  });
+
+  const words = text.split(" ");
+
+  return (
+    <div className="text-5xl font-bold h-[200vh] flex items-center justify-center bg-slate-50">
+      <Container>
+        <p ref={element} className="flex flex-wrap">
+          {words.map((word, idx) => {
+            const start = idx / words.length;
+            const end = start + 1 / words.length;
+            const isLastWord = idx === words.length - 1;
+
+            return (
+              <Word
+                key={idx}
+                range={[start, end]}
+                progress={scrollYProgress}
+                isLastWord={isLastWord}
+              >
+                {word}
+              </Word>
+            );
+          })}
+        </p>
+      </Container>
+    </div>
+  );
+};
+
+export default LandingParagraph;
+
+const Word = ({ children, range, progress, isLastWord }: LandingWordsProps) => {
+  const opacity = useTransform(progress, range, [0, 1]);
+
+  const createTransform = (outputRange: string[]) =>
+    isLastWord
+      ? useTransform(
+          progress,
+          [range[0], range[1] * 0.95, range[1]],
+          outputRange
+        )
+      : outputRange[0];
+
+  const color = createTransform(["#000", "#000", "#fff"]);
+  const backgroundColor = createTransform(["#fff", "#fff", "#000"]);
+  const textDecoration = createTransform(["none", "none", "underline"]);
+  const textUnderlineOffset = createTransform(["auto", "auto", "4px"]);
+
+  return (
+    <span className="flex flex-wrap relative">
+      <span className="absolute opacity-[0.1] p-2 pb-3">{children}</span>
+      <motion.span
+        style={{
+          opacity,
+          color,
+          backgroundColor,
+          textDecoration,
+          textUnderlineOffset,
+        }}
+        className="p-2 pb-3"
+      >
+        {children}
+      </motion.span>
+    </span>
+  );
+};
