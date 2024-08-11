5d296652c252e51d426dc31155c376c877878b59
sanchxt <tpbeast007@gmail.com>
2024-08-11 23:26:17 +0530

    implement landing page's components

--- frontend/src/pages/LandingPage.tsx
+++ frontend/src/pages/LandingPage.tsx
@@ -1,22 +1,21 @@
-import Container from "../components/landing-page/Container";
-import LandingHeader from "../components/landing-page/LandingHeader";
 import LandingHero from "../components/landing-page/LandingHero";
+import LandingUsps from "../components/landing-page/LandingUsps";
+import LandingHeader from "../components/landing-page/LandingHeader";
+import LandingCarousel from "../components/landing-page/LandingCarousel";
+import LandingParagraph from "../components/landing-page/LandingParagraph";
 
 const LandingPage = () => {
   return (
     <>
       <LandingHeader />
       <main>
-        <LandingHero />
-        <div>
-          <Container>USPs</Container>
+        <div className="bg-black relative z-10">
+          <LandingHero />
+          <LandingUsps />
         </div>
-        <div>
-          <Container>3 col layout</Container>
-        </div>
-        <div>
-          <Container>Carousel with posters</Container>
-        </div>
+        <LandingCarousel />
+        <LandingParagraph text="We give you the head start that everyone wishes they had. No more pulling your hair out of confusion, trying to find the perfect pathway. We solve that problem for you." />
+        {/* <div className="h-[300vh]"></div> */}
       </main>
     </>
   );
