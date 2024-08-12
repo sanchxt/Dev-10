d272f1a52b6f112435e3548810471eeb06d89124
sanchxt <tpbeast007@gmail.com>
2024-08-13 03:15:03 +0530

    implement main box component

--- frontend/src/components/revamped-home/HomePage.tsx
+++ frontend/src/components/revamped-home/HomePage.tsx
@@ -1,7 +1,8 @@
 import { useSelector } from "react-redux";
-import { RootState } from "../../store";
+
 import HomeHeader from "./HomeHeader";
-import "../../assets/home_layout.css";
+import MainInfoBox from "./MainInfoBox";
+import { RootState } from "../../store";
 
 const HomePage = () => {
   const userInfo = useSelector((state: RootState) => state.auth.userInfo);
@@ -9,9 +10,11 @@ const HomePage = () => {
   return (
     <div className="bg-slate-100 h-full">
       <HomeHeader name={userInfo?.name!} />
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-[60%_40%]">
-        <div className="bg-slate-300 grid grid-cols-2">
-          <div className="bg-blue-100 col-span-2">main banner</div>
+      <div className="grid grid-cols-1 md:grid-cols-[57%_43%] lg:grid-cols-[60%_40%]">
+        <div className="grid grid-cols-2">
+          <div className="col-span-2 px-0.5 py-2 md:px-2 md:py-3">
+            <MainInfoBox />
+          </div>
           <div className="col-span-2 grid grid-cols-1 lg:grid-cols-2 bg-blue-200">
             <div className="bg-green-100 order-2 md:order-1">contris</div>
             <div className="bg-green-200 order-1 md:order-2">sponsors</div>
