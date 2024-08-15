8937b7a8cd7ce20ae88c103dd97cbf0d3ea3881b
sanchxt <tpbeast007@gmail.com>
2024-08-16 00:19:49 +0530

    revamp total contributions component

--- frontend/src/components/revamped-home/DisplayTotalContributions.tsx
+++ frontend/src/components/revamped-home/DisplayTotalContributions.tsx
@@ -18,43 +18,47 @@ const DisplayTotalContributions = () => {
   }
 
   return (
-    <div className="grid grid-cols-2 gap-y-4">
-      <div className="grid gap-4">
+    <div className="grid grid-rows-[1fr_1fr_1fr] py-2 gap-y-4 h-full">
+      <div className="grid grid-cols-2">
         <h3 className="text-center italic text-sm md:text-base lg:text-lg font-light">
           Ratings
         </h3>
-        <p className="text-center font-black text-2xl md:text-3xl lg:text-4xl">
-          {data?.averageRating.toFixed(1) || "N/A"}
-        </p>
-      </div>
-      <div className="grid gap-4">
+
         <h3 className="text-center italic text-sm md:text-base lg:text-lg font-light">
           Contributions
         </h3>
-        <p className="text-center font-black text-2xl md:text-3xl lg:text-4xl">
-          {data?.totalContributions || "N/A"}
-        </p>
       </div>
 
-      <p className="col-span-2 text-center italic font-extralight text-[0.67rem] sm:text-xs lg:text-sm p-1 text-gray-700">
-        Give back to the community by contributing your own{" "}
-        <Link
-          to="/resources"
-          className="text-blue-600 inline-flex font-semibold relative group cursor-pointer hover:text-black hover:font-black transition-all duration-500 ease-in-out"
-        >
-          resources
-          <span className="absolute left-0 top-0 h-full w-0 group-hover:w-full bg-purple-300/40 transition-all duration-500 ease-in-out"></span>
-        </Link>{" "}
-        or{" "}
-        <Link
-          to="/roadmaps"
-          className="text-blue-600 inline-flex font-semibold relative group cursor-pointer hover:text-black hover:font-black transition-all duration-500 ease-in-out"
-        >
-          roadmaps
-          <span className="absolute left-0 top-0 h-full w-0 group-hover:w-full bg-purple-300/40 transition-all duration-500 ease-in-out"></span>
-        </Link>{" "}
-        for other developers to access.
-      </p>
+      <div className="bg-slate-200 grid grid-cols-2">
+        <h2 className="flex justify-center items-start font-black text-3xl sm:text-4xl lg:text-5xl">
+          {data?.averageRating.toFixed(1) || "N/A"}
+        </h2>
+        <h2 className="flex justify-center items-start font-black text-3xl sm:text-4xl lg:text-5xl">
+          {data?.totalContributions || "N/A"}
+        </h2>
+      </div>
+
+      <div>
+        <p className="col-span-2 text-center italic font-extralight text-[0.67rem] sm:text-xs lg:text-sm p-1 text-gray-700">
+          Give back to the community by contributing your own{" "}
+          <Link
+            to="/resources"
+            className="text-blue-600 inline-flex font-semibold relative group cursor-pointer hover:text-black hover:font-black transition-all duration-500 ease-in-out"
+          >
+            resources
+            <span className="absolute left-0 top-0 h-full w-0 group-hover:w-full bg-purple-300/40 transition-all duration-500 ease-in-out"></span>
+          </Link>{" "}
+          or{" "}
+          <Link
+            to="/roadmaps"
+            className="text-blue-600 inline-flex font-semibold relative group cursor-pointer hover:text-black hover:font-black transition-all duration-500 ease-in-out"
+          >
+            roadmaps
+            <span className="absolute left-0 top-0 h-full w-0 group-hover:w-full bg-purple-300/40 transition-all duration-500 ease-in-out"></span>
+          </Link>{" "}
+          for other developers to access.
+        </p>
+      </div>
     </div>
   );
 };
