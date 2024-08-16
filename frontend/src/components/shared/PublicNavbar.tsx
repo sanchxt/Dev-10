6a9d78d30d2e142638debbcff7ce46b57609954c
sanchxt <tpbeast007@gmail.com>
2024-08-17 00:54:30 +0530

    logo size fix

--- frontend/src/components/shared/PublicNavbar.tsx
+++ frontend/src/components/shared/PublicNavbar.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { BiMenu, BiMoon, BiX } from "react-icons/bi";
 import { useSelector, useDispatch } from "react-redux";
 
-import logo from "../../../public/Dev10-Logo2.png";
+import logo from "/Dev10-Logo2.png";
 import { RootState } from "../../store";
 import { navItems } from "../../utils/constants";
 import { setTheme } from "../../slices/themeSlice";
@@ -27,12 +27,8 @@ const PublicNavbar = () => {
       } md:shadow-none theme-transition`}
     >
       <nav className="flex justify-between items-center p-1 sm:w-[92%] mx-auto">
-        <div className="">
-          <img
-            src={logo}
-            alt="Dev-10 Logo"
-            className="w-[4.5rem] md:w-20 xl:w-4"
-          />
+        <div className="w-[2rem] sm:w-12 md:w-12 lg:w-16 xl:w-20">
+          <img src={logo} alt="Dev-10 Logo" className="object-contain" />
         </div>
 
         <div
