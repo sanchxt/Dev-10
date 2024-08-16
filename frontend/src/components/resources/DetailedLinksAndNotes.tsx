84699807c55a6a7a34df160b78d5a7aaff51ea17
sanchxt <tpbeast007@gmail.com>
2024-08-16 21:31:52 +0530

    update links and notes structure

--- frontend/src/components/resources/DetailedLinksAndNotes.tsx
+++ frontend/src/components/resources/DetailedLinksAndNotes.tsx
@@ -4,59 +4,33 @@ import { addHttpPrefix } from "../../utils/helpers";
 import { DetailedLinksAndNotesProps } from "../../utils/types";
 
 const DetailedLinksAndNotes = ({
-  essentials,
-  extras,
+  links,
   notes,
 }: DetailedLinksAndNotesProps) => {
+  console.log("links:", links);
+
   return (
     <div className="flex md:gap-1 flex-wrap md:flex-nowrap p-0.5 max-h-[30rem]">
       {/* essentials */}
-      <div className="w-1/2 md:w-1/3 flex flex-col py-2 xl:py-4">
+      <div className="w-1/2 flex flex-col py-2 xl:py-4">
         <h2 className="w-full h-fit italic text-center tracking-wide font-light text-sm md:text-base lg:text-lg xl:text-2xl">
-          Essentials
+          All Links
         </h2>
 
         <ul className="w-full text-left font-thin tracking-wider text-[0.68rem] md:text-xs lg:text-sm 2xl:text-base pt-3 md:pt-6 xl:pt-8 px-0.5 md:px-2 grid gap-2">
-          {essentials?.map((link: string, idx: number) => (
+          {links?.map((link: any, idx: number) => (
             <li
               key={idx}
               className="bg-purple-200 group px-2 py-0.5 md:py-1 overflow-hidden text-ellipsis whitespace-pre rounded-xl hover:shadow-md transition-all duration-300 ease-in-out cursor-pointer"
             >
               <a
-                href={addHttpPrefix(link)}
+                href={addHttpPrefix(link.url)}
                 rel="noreferrer"
                 target="_blank"
                 className="block"
               >
                 <p className="inline relative after:bg-slate-600 after:absolute after:h-[0.1rem] after:w-0 after:bottom-0 after:left-0 group-hover:after:w-full after:transition-all after:duration-500">
-                  {link}
-                </p>
-              </a>
-            </li>
-          ))}
-        </ul>
-      </div>
-
-      {/* extras */}
-      <div className="w-1/2 md:w-1/3 flex flex-col py-2 xl:py-4">
-        <h2 className="w-full h-fit italic text-center tracking-wide font-light text-sm md:text-base lg:text-lg xl:text-2xl">
-          Extras
-        </h2>
-
-        <ul className="w-full text-left font-light tracking-wider text-[0.68rem] md:text-xs lg:text-sm 2xl:text-base pt-3 md:pt-6 xl:pt-8 px-0.5 md:px-2 flex flex-col gap-2">
-          {extras?.map((link: string, idx: number) => (
-            <li
-              key={idx}
-              className="bg-purple-200 group px-2 py-0.5 md:py-1 overflow-hidden text-ellipsis whitespace-nowrap rounded-xl hover:shadow-md transition-all duration-300 ease-in-out cursor-pointer"
-            >
-              <a
-                href={addHttpPrefix(link)}
-                rel="noreferrer"
-                target="_blank"
-                className="block"
-              >
-                <p className="inline relative after:bg-slate-600 after:absolute after:h-[0.1rem] after:w-0 after:bottom-0 after:left-0 group-hover:after:w-full after:transition-all after:duration-500">
-                  {link}
+                  {link.url}
                 </p>
               </a>
             </li>
@@ -65,7 +39,7 @@ const DetailedLinksAndNotes = ({
       </div>
 
       {/* notes */}
-      <div className="w-full md:w-1/3 flex flex-col py-2 xl:py-4">
+      <div className="w-1/2 flex flex-col py-2 xl:py-4">
         <h2 className="w-full h-fit text-center tracking-wide font-normal text-sm md:text-base lg:text-lg xl:text-2xl">
           Author Notes
         </h2>
